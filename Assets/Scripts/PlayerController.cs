819322dbc354551055b6d0eb58444b1f998e5e12
Timo <Timo.1102@web.de>
2015-07-09 12:47:10 +0200

    Add Projectil

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,7 +3,11 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
+    
+    public GameObject Projectil;
+
     public float rotateSpeed = 8.0f;
+    public float ProjectilSpeed = 8.0f;
 
 	// Use this for initialization
 	void Start () {
@@ -18,6 +22,13 @@ public class PlayerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+           var prefab = Instantiate(Projectil, this.transform.position, Quaternion.identity) as GameObject;
+           prefab.GetComponent<Projectil>().speed = ProjectilSpeed;
+        }
+
+
+
 	}
 }
---
+++ Assets/Scripts/Projectil.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Projectil : MonoBehaviour {
+
+    public float speed;
+	// Use this for initialization
+
+	
+	// Update is called once per frame
+	void Update () {
+      
+        if (this.transform.position != Vector3.zero)
+        {
+            this.transform.position = Vector3.MoveTowards(this.transform.position, Vector3.zero, speed * Time.deltaTime);
+        }else
+        {
+            Destroy(this.gameObject);
+        }
+
+	}
+}
